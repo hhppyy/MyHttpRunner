b020af9527ccdbe05f15ed5de8a7cc362a7bbce0
hhppyy <741841851@qq.com>
2021-04-16 18:12:39 +0800

    第三次提交

--- debugtalk.py
+++ debugtalk.py
@@ -3,3 +3,20 @@ import time
 
 def sleep(n_secs):
     time.sleep(n_secs)
+
+
+def setup_hooks(method,url,**kwargs):
+    """
+    - method: 请求方法，e.g. GET, POST, PUT
+    - url: 请求 URL
+    - kwargs: request 的参数字典
+    :return:
+    """
+    pass
+
+def teardown_hooks(resp_obj):
+    """
+    - resp_obj: requests.Response 实例
+    :return:
+    """
+    pass
