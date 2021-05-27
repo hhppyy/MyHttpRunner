6e15c4da72f27a88a12906f09230266f09b7170a
hhppyy <741841851@qq.com>
2021-05-27 17:15:52 +0800

    第十一次提交

--- debugtalk.py
+++ debugtalk.py
@@ -1,11 +1,11 @@
-
 import time
+
 
 def sleep(n_secs):
     time.sleep(n_secs)
 
 
-def setup_hooks(method,url,**kwargs):
+def setup_hooks(method, url, **kwargs):
     """
     - method: 请求方法，e.g. GET, POST, PUT
     - url: 请求 URL
@@ -14,9 +14,28 @@ def setup_hooks(method,url,**kwargs):
     """
     pass
 
+
 def teardown_hooks(resp_obj):
     """
     - resp_obj: requests.Response 实例
     :return:
     """
     pass
+
+
+def get_file(filepath):
+    """
+    上传图片函数
+    :param filepath: 路径
+    :return: file-like-objects
+    """
+    with open(filepath, 'rb') as f:
+        return f
+
+    # return open(filepath, 'rb')
+
+
+if __name__ == '__main__':
+    a = r'E:\MyHttpRunner\data\kenan.jpg'
+    v = get_file(a)
+    print(v)
